15117a8aad3bc71670518269105d7572b1d3f928
Teemmer <dziga2000@gmail.com>
2020-03-17 03:09:40 +0200

    Refactoring

--- ngfg.client/src/components/Field/FieldsPage.js
+++ ngfg.client/src/components/Field/FieldsPage.js
@@ -26,80 +26,86 @@ class Filter extends Component {
             <div className="filter-item">
                 <h3 className='filter-category'>Type</h3>
                 <FormGroup className='filter-typo'>
-                    <FormControlLabel className='filter-typo'
-                                      control={
-                                          <Checkbox checked={this.props.filter.showNumber}
-                                                    onChange={(event) => this.onChange(
-                                                        event,
-                                                        {
-                                                            showNumber: !this.props.filter.showNumber,
-                                                            showAll: false
-                                                        })}
-                                          />}
-                                      label="Number"
+                    <FormControlLabel
+                        className='filter-typo'
+                        control={
+                            <Checkbox checked={this.props.filter.showNumber}
+                                      onChange={(event) => this.onChange(
+                                          event,
+                                          {
+                                              showNumber: !this.props.filter.showNumber,
+                                              showAll: false
+                                          })}
+                            />}
+                        label="Number"
                     />
-                    <FormControlLabel className='filter-typo'
-                                      control={
-                                          <Checkbox checked={this.props.filter.showText}
-                                                    onChange={(event) => this.onChange(
-                                                        event,
-                                                        {
-                                                            showText: !this.props.filter.showText,
-                                                            showAll: false
-                                                        })}
-                                          />}
-                                      label="Text"
+                    <FormControlLabel
+                        className='filter-typo'
+                        control={
+                            <Checkbox checked={this.props.filter.showText}
+                                      onChange={(event) => this.onChange(
+                                          event,
+                                          {
+                                              showText: !this.props.filter.showText,
+                                              showAll: false
+                                          })}
+                            />}
+                        label="Text"
                     />
-                    <FormControlLabel className='filter-typo'
-                                      control={
-                                          <Checkbox checked={this.props.filter.showTextArea}
-                                                    onChange={(event) => this.onChange(
-                                                        event,
-                                                        {
-                                                            showTextArea: !this.props.filter.showTextArea,
-                                                            showAll: false
-                                                        })}
-                                          />}
-                                      label="TextArea"
+                    <FormControlLabel
+                        className='filter-typo'
+                        control={
+                            <Checkbox checked={this.props.filter.showTextArea}
+                                      onChange={(event) => this.onChange(
+                                          event,
+                                          {
+                                              showTextArea: !this.props.filter.showTextArea,
+                                              showAll: false
+                                          })}
+                            />}
+                        label="TextArea"
                     />
-                    <FormControlLabel className='filter-typo'
-                                      control={
-                                          <Checkbox checked={this.props.filter.showCheckbox}
-                                                    onChange={(event) => this.onChange(
-                                                        event,
-                                                        {
-                                                            showCheckbox: !this.props.filter.showCheckbox,
-                                                            showAll: false
-                                                        })}
-                                          />
-                                      }
-                                      label="Checkbox"
+                    <FormControlLabel
+                        className='filter-typo'
+                        control={
+                            <Checkbox checked={this.props.filter.showCheckbox}
+                                      onChange={(event) => this.onChange(
+                                          event,
+                                          {
+                                              showCheckbox: !this.props.filter.showCheckbox,
+                                              showAll: false
+                                          })}
+                            />
+                        }
+                        label="Checkbox"
                     />
-                    <FormControlLabel className='filter-typo'
-                                      control={
-                                          <Checkbox checked={this.props.filter.showRadio}
-                                                    onChange={(event) => this.onChange(
-                                                        event,
-                                                        {
-                                                            showRadio: !this.props.filter.showRadio,
-                                                            showAll: false
-                                                        })}
-                                          />
-                                      }
-                                      label="Radio"
+                    <FormControlLabel
+                        className='filter-typo'
+                        control={
+                            <Checkbox checked={this.props.filter.showRadio}
+                                      onChange={(event) => this.onChange(
+                                          event,
+                                          {
+                                              showRadio: !this.props.filter.showRadio,
+                                              showAll: false
+                                          })}
+                            />
+                        }
+                        label="Radio"
                     />
-                    <FormControlLabel className='filter-typo'
-                                      control={
-                                          <Checkbox checked={this.props.filter.showAutocomplete}
-                                                    onChange={(event) => this.onChange(
-                                                        event,
-                                                        {
-                                                            showAutocomplete: !this.props.filter.showAutocomplete,
-                                                            showAll: false
-                                                        })}
-                                          />
-                                      }
-                                      label="Autocomplete"
+                    <FormControlLabel
+                        className='filter-typo'
+                        control={
+                            <Checkbox checked={this.props.filter.showAutocomplete}
+                                      onChange={(event) => this.onChange(
+                                          event,
+                                          {
+                                              showAutocomplete: !this.props.filter.showAutocomplete,
+                                              showAll: false
+                                          })}
+                            />
+                        }
+                        label="Autocomplete"
                     />
                 </FormGroup>
             </div>
