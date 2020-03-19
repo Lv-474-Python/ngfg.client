067aaa8c3a8b486ad8e89a12a84921229237dae9
AndriiKret <andrii.kret@gmail.com>
2020-03-19 15:11:43 +0200

    Optimize imports

--- ngfg.client/src/components/Field/CreationWindow.js
+++ ngfg.client/src/components/Field/CreationWindow.js
@@ -1,11 +1,8 @@
 import CreateField from './CreateField'
 
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
+
+import {Button, Dialog, DialogActions, DialogContent, DialogTitle} from '@material-ui/core';
 
 export default function FormDialog(props) {
     const [open, setOpen] = React.useState(false);
