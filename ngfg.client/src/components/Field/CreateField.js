5ec304166a63fe3ba42ab048da7b763ef5af7f51
AndriiKret <59411736+AndriiKret@users.noreply.github.com>
2020-03-19 16:03:42 +0200

    Added create field to home_page and /fields (HotFix#9) (#22)
    * Added create field to home_page and /fields
    
    * Optimize imports

--- ngfg.client/src/components/Field/CreateField.js
+++ ngfg.client/src/components/Field/CreateField.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import './Field.css'
 
-import Popup from "reactjs-popup";
 import CreateNumberOrTextField from './CreateNumberOrTextField'
 import CreateTextArea from './CreateTextArea'
 import CreateMultiChoice from './CreateMultiChoice'
@@ -24,44 +23,28 @@ class CreateField extends Component {
     render() {
         return (
             <div>
-                <Popup trigger={<button className="button"> Create Field </button>} modal>
-                    {close => (
-                        <div className="modal">
-                            <a className="close" onClick={close}>
-                                &times;
-                            </a>
-                            <div className="header"> Create field</div>
-                            <div className="content">
-                                <SelectFieldType onChange={this.handleChangeFieldType}
-                                                 fieldType={this.state.fieldType}
-                                />
-                                {
-                                    [1].includes(this.state.fieldType) && <CreateNumberOrTextField fieldType={1}/>
-                                }
-                                {
-                                    [2].includes(this.state.fieldType) && <CreateNumberOrTextField fieldType={2}/>
-                                }
-                                {
-                                    [3].includes(this.state.fieldType) && <CreateTextArea/>
-                                }
-                                {
-                                    [4].includes(this.state.fieldType) && <CreateMultiChoice fieldType={4}/>
-                                }
-                                {
-                                    [5].includes(this.state.fieldType) && <CreateSettingAutocompleteField/>
-                                }
-                                {
-                                    [6].includes(this.state.fieldType) && <CreateMultiChoice fieldType={6}/>
-                                }
-                            </div>
-                            <div className="actions">
-
-                            </div>
-                        </div>
-                    )}
-                </Popup>
+                <SelectFieldType onChange={this.handleChangeFieldType}
+                                 fieldType={this.state.fieldType}
+                />
+                {
+                    [1].includes(this.state.fieldType) && <CreateNumberOrTextField fieldType={1}/>
+                }
+                {
+                    [2].includes(this.state.fieldType) && <CreateNumberOrTextField fieldType={2}/>
+                }
+                {
+                    [3].includes(this.state.fieldType) && <CreateTextArea/>
+                }
+                {
+                    [4].includes(this.state.fieldType) && <CreateMultiChoice fieldType={4}/>
+                }
+                {
+                    [5].includes(this.state.fieldType) && <CreateSettingAutocompleteField/>
+                }
+                {
+                    [6].includes(this.state.fieldType) && <CreateMultiChoice fieldType={6}/>
+                }
             </div>
-
         );
     }
 }
--- ngfg.client/src/components/HomePage/NewFieldItem.js
+++ ngfg.client/src/components/HomePage/NewFieldItem.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 
 import './NewFieldItem.scss';
 
+import CreateWindow from '../Field/CreationWindow'
+
 
 // може винести FormNewItem i NewFieldItem -> в NewItem
 class NewFieldItem extends Component {
 
     render() {
         return (
-            <div className="new-field-item">
-                +
-            </div>
+                <CreateWindow text={'+'}/>
         );
     }
 }
