08fcb2cd095b867eafb785baacc28c9b8d43fd8a
vitaliiaol <vitaliiya.ol@gmail.com>
2020-03-19 13:40:12 +0200

    Another little fix

--- ngfg.client/src/components/HomePage/NewFormItem.js
+++ ngfg.client/src/components/HomePage/NewFormItem.js
@@ -11,7 +11,6 @@ class NewFormItem extends Component {
     }
 
     render() {
-        console.log(this.props)
         return (
             <div className="new-form-item"
                  onClick={this.handleClick}>
