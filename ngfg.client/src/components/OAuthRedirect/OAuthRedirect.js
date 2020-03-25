33d393931dee69cd9b404619725a65ed6c551ca7
IvanKostiv <ivankostiv.pz@gmail.com>
2020-03-25 18:35:32 +0200

    Fixed

--- ngfg.client/src/components/OAuthRedirect/OAuthRedirect.js
+++ ngfg.client/src/components/OAuthRedirect/OAuthRedirect.js
@@ -1,14 +1,13 @@
 import React, {Component} from "react";
 import {withRouter} from "react-router-dom";
 import axios from 'axios'
-import {brown} from "@material-ui/core/colors";
 
 const CALLBACK_URI = 'http://ngfg.com:8000/api/v1/auth/login/callback';
 
 class OAuthRedirect extends Component {
     getSession = () => {
         axios.get(CALLBACK_URI + this.props.location.search, {
-            withCredentials: true,
+            withCredentials: true
         }).then((res) => {
             window.location.href = '/'
         }).catch((error) => {
