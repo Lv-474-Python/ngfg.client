9e693756aa8a3b5b5b7b4473cab540bf5e237a64
vitaliiaol <vitaliiya.ol@gmail.com>
2020-03-31 21:11:11 +0300

    Adding questions

--- ngfg.client/src/components/Form/AdditionalComponent/FormFieldCreationList.js
+++ ngfg.client/src/components/Form/AdditionalComponent/FormFieldCreationList.js
@@ -1,10 +1,34 @@
 import React, {Component} from "react";
 import CreateFormField from "../../Field/CreateFormField";
+import {CardContent, TextField} from "@material-ui/core";
 
 class FormFieldCreationList extends Component {
 
     state = {
-        fields: this.props.fields
+        questions: []
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.fields.length != this.props.fields.length || this.detectQuestionsUpdate(prevState, this.state)) {
+            let formFields = [];
+            this.props.fields.map((elem, index) =>
+                formFields[index] = {fieldId: elem.id, position: index, question: this.state.questions[index]}
+            );
+            this.props.addFormFields(formFields);
+        }
+    }
+
+    detectQuestionsUpdate = (prevState, thisState) => {
+        this.props.fields.map(function(elem, index) {
+
+        if(prevState.questions[index]!=thisState.questions[index]){
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    });
     }
 
     handleSendField = (fieldId, position, question) => {
@@ -23,11 +47,26 @@ class FormFieldCreationList extends Component {
         this.props.moveDownField(position, disabled);
     }
 
+    addQuestion = (event, position) => {
+        //let questions = this.state.questions;
+        //questions[position] = event.target.value;
+        this.props.fetchQuestions(position, event.target.value)
+    }
+
     render() {
+        console.log(this.state.questions)
         return(
-            <div>
+            <React.Fragment>
             {
                 this.props.fields.map((elem, index) =>
+                        <div>
+                            <TextField variant="outlined"
+                                       helperText={`Enter question for ${elem.name}`}
+                                       size="small"
+                                       type="text"
+                                       value={elem.question}
+                                       className="form-creation-fields"
+                                       onChange={(event) => this.addQuestion(event, index)}/>
                         <CreateFormField field={elem}
                                          id={elem.id}
                                          position={index}
@@ -37,9 +76,9 @@ class FormFieldCreationList extends Component {
                                          onMoveDownClick={this.handleMoveDownField}
                                          disableMoveUp={index==0 ? true : false}
                                          disableMoveDown={index==this.props.fields.length-1 ? true : false}
-                        />)
+                        /></div>)
             }
-            </div>
+            </React.Fragment>
         )
     }
 
