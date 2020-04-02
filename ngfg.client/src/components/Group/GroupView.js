782e59d16de9a895f7cc52650a5df76278e02264
Mykola Matviiv <matviivcareer@gmail.com>
2020-04-02 22:43:15 +0300

    group page

--- ngfg.client/src/components/Group/GroupView.js
+++ ngfg.client/src/components/Group/GroupView.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import axios from 'axios';
+import {Button, Card, CardActions, CardHeader, CardContent, Typography} from "@material-ui/core";
+import './GroupView.css'
 
 const API_URL = 'http://ngfg.com:8000/api';
 const API_VERSION = 'v1';
@@ -20,9 +22,20 @@ class GroupView extends Component {
         })
             .then(res => {
                 const group = res.data;
-                this.setState({ ...group })
+                this.setState({...group}, this.getOwnerName)
             })
     }
+
+    getOwnerName = () => {
+        console.log(this.state.ownerId)
+        axios.get(`${API_URL}/${API_VERSION}/users/${this.state.ownerId}`, {
+            withCredentials: true,
+        })
+            .then(res => {
+                const ownerName = res.data.username;
+                this.setState({ownerName})
+            });
+    };
 
     componentDidMount() {
         this.getGroupData();
@@ -31,11 +44,64 @@ class GroupView extends Component {
     render() {
         return (
             <div>
-                id {this.state.id},
-                name {this.state.name},
-                {this.state.users.map(elem =>
-                    elem.email
-                )}
+                <div className={'Test'}>
+                    <Card className='group-view'>
+                        {/*<CardHeader*/}
+                        {/*        title={this.state.name}*/}
+                        {/*/>*/}
+
+                        <CardContent>
+
+                            <Typography variant="h3"
+                                        component="h1"
+                                        className='group-view-header'>
+
+                                {this.state.name}
+
+                            </Typography>
+
+                            <Typography variant="h6"
+                                        component="h2"
+                                        className='group-view-owner'>
+
+                                owner name {this.state.ownerName}
+
+
+                            </Typography>
+
+
+                            <Typography variant="h6"
+                                        component="h2"
+                                        className='group-view-created'>
+
+                                created: {this.state.created}
+
+                            </Typography>
+
+                            <Typography variant="h6"
+                                        component="h2"
+                                        className='group-view-members'>
+                                MEMbers:
+
+                                <ol className={"member-list"}>
+                                    {this.state.users.map((item, i) =>
+                                        <li> {item.username} {item.email} </li>
+                                    )}
+                                </ol>
+
+                            </Typography>
+                        </CardContent>
+
+                        <CardActions>
+                            <Button
+                                className='group-view-link'
+                                size="medium"
+                                onClick={this.goToView}>
+                                View group
+                            </Button>
+                        </CardActions>
+                    </Card>
+                </div>
             </div>
         );
     }
