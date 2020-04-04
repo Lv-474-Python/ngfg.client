86507abf7dac603f856a2b5ee3eaacc0957ef748
Mykola Matviiv <matviivcareer@gmail.com>
2020-04-04 18:12:17 +0300

    under development

--- ngfg.client/src/components/Group/GroupView.js
+++ ngfg.client/src/components/Group/GroupView.js
@@ -1,6 +1,23 @@
 import React, {Component} from 'react';
 import axios from 'axios';
-import {Button, Card, CardActions, CardHeader, CardContent, Typography} from "@material-ui/core";
+import {
+    Button,
+    Card,
+    CardActions,
+    CardHeader,
+    CardContent,
+    Typography,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TablePagination,
+    TableRow
+} from "@material-ui/core";
+
+
 import './GroupView.css'
 
 const API_URL = 'http://ngfg.com:8000/api';
@@ -64,42 +81,65 @@ class GroupView extends Component {
                                         component="h2"
                                         className='group-view-owner'>
 
-                                owner name {this.state.ownerName}
-
-
-                            </Typography>
-
-
-                            <Typography variant="h6"
-                                        component="h2"
-                                        className='group-view-created'>
-
-                                created: {this.state.created}
+                            <div className={'info'}>
+                                <span className={'owner'}>owner name {this.state.ownerName}</span>
+                                <span className={'created'}>created: {this.state.created}</span>
+                            </div>
 
                             </Typography>
 
-                            <Typography variant="h6"
-                                        component="h2"
-                                        className='group-view-members'>
-                                MEMbers:
 
-                                <ol className={"member-list"}>
-                                    {this.state.users.map((item, i) =>
-                                        <li> {item.username} {item.email} </li>
-                                    )}
-                                </ol>
+                            {/*<Typography variant="h6"*/}
+                            {/*            component="h2"*/}
+                            {/*            className='group-view-created'>*/}
 
-                            </Typography>
+                            {/*    /!*created: {this.state.created}*!/*/}
+
+                            {/*</Typography>*/}
+
+                            {/*<Typography variant="h6"*/}
+                            {/*            component="h2"*/}
+                            {/*            className='group-view-members'>*/}
+                            {/*    MEMbers:*/}
+
+                            {/*<ol className={"member-list"}>*/}
+                            {/*    {this.state.users.map((item, i) =>*/}
+                            {/*        <li> {item.username} {item.email} </li>*/}
+                            {/*    )}*/}
+                            {/*</ol>*/}
+
+                            <TableContainer component={Paper}>
+                                <Table className="member" aria-label="simple table">
+                                    <TableHead className='member-head'>
+                                        <TableRow className={'member-row'}>
+                                            <TableCell align={"center"}>#</TableCell>
+                                            <TableCell align={"center"}>Name</TableCell>
+                                            <TableCell align={"center"}>Email</TableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody className={'member-body'}>
+                                        {this.state.users.map((row, index) => (
+                                            <TableRow component="tr" key={row.username}>
+                                                <TableCell align={"center"}
+                                                           component="td"
+                                                           scope="row">
+                                                    {index + 1}
+                                                </TableCell>
+                                                <TableCell
+                                                    align={"center"}>{row.username}</TableCell>
+                                                <TableCell align={"center"}>{row.email}</TableCell>
+
+
+                                            </TableRow>
+                                        ))}
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
+
+
+                            {/*</Typography>*/}
                         </CardContent>
 
-                        <CardActions>
-                            <Button
-                                className='group-view-link'
-                                size="medium"
-                                onClick={this.goToView}>
-                                View group
-                            </Button>
-                        </CardActions>
                     </Card>
                 </div>
             </div>
