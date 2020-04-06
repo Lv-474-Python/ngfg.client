7a4a1c6ffac1e905ae0d185f700cc957ceb95487
Mykola Matviiv <matviivcareer@gmail.com>
2020-04-07 01:33:34 +0300

    change scroll to pagination

--- ngfg.client/src/components/Group/GroupView.js
+++ ngfg.client/src/components/Group/GroupView.js
@@ -30,7 +30,7 @@ class GroupView extends Component {
         "ownerName": undefined,
         "users": [],
         "page": 1,
-        "rowsPerPage": 10
+        "rowsPerPage": 5
     }
 
     formatDate = (date) => {
@@ -64,12 +64,12 @@ class GroupView extends Component {
     };
 
     handleChangePage = (newPage, pageNumber) => {
-        this.setState({page : pageNumber + 1});
+        this.setState({page: pageNumber + 1});
     };
 
     handleChangeRowsPerPage = (event) => {
         console.log(event.target.value)
-        this.setState({rowsPerPage : event.target.value, page :1});
+        this.setState({rowsPerPage: event.target.value, page: 1});
     };
 
     componentDidMount() {
@@ -77,64 +77,86 @@ class GroupView extends Component {
     }
 
     render() {
-        console.log(this.state.page)
-        console.log(this.state.rowsPerPage)
-        console.log(this.state.users)
-        console.log(this.state.users.slice((this.state.page - 1) * this.state.rowsPerPage,
-                                this.state.page * this.state.rowsPerPage))
-        console.log((this.state.page - 1) * this.state.rowsPerPage)
-        console.log(this.state.page * this.state.rowsPerPage)
-
         return (
-            <Paper className={"root"}>
-                <TableContainer className={"container"}>
-                    <Table stickyHeader aria-label="sticky table">
-                        <TableHead>
-                            <TableRow>
-                                {columns.map((column) => (
-                                    <TableCell
-                                        key={column.index}
-                                        align={"center"}
-                                    >
-                                        {column}
-                                    </TableCell>
-                                ))}
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {this.state.users.slice((this.state.page -1)* this.state.rowsPerPage,
-                                this.state.page * this.state.rowsPerPage).map((row, index) => {
-                                return (
-                                    <TableRow component="tr" key={row.id}>
-                                    <TableCell align={"center"}
-                                               component="td"
-                                               scope="row">
-                                        {index + 1}
-                                    </TableCell>
-                                    <TableCell
-                                        align={"center"}>{row.username}</TableCell>
-                                    <TableCell
-                                        align={"center"}>{row.email}</TableCell>
+            <div>
+                <div className={'Test'}>
+                    <Card className={'group-view'}>
+                        <CardContent>
+                            <Typography variant="h3"
+                                        component="h1"
+                                        className='group-view-header'>
+                                {this.state.name}
+                            </Typography>
 
+                            <Typography variant="h6"
+                                        component="h2"
+                                        className='group-view-owner'>
+                                <div className={'info'}>
+                                    <span
+                                        className={'owner'}>Owner name: {this.state.ownerName}</span>
+                                    <span className={'created'}>Created: {this.state.created}</span>
+                                </div>
+                            </Typography>
 
-                                </TableRow>
-                                )
+                            <h2>Group Members: </h2>
 
-                            })
-                            }
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-                <TablePagination
-                    rowsPerPageOptions={[5, 10, 25]}
-                    component="div"
-                    count={this.state.users.length}
-                    rowsPerPage={this.state.rowsPerPage}
-                    page={this.state.page -1}
-                    onChangePage={this.handleChangePage}
-                    onChangeRowsPerPage={this.handleChangeRowsPerPage}
-                />
-            </Paper>
+                            {this.state.users.length !== 0 ?
+                                <Paper className={"root"}>
+                                    <TableContainer className={"container"}>
+                                        <Table stickyHeader aria-label="sticky table">
+                                            <TableHead>
+                                                <TableRow>
+                                                    {columns.map((column) => (
+                                                        <TableCell
+                                                            key={column.index}
+                                                            align={"center"}
+                                                        >
+                                                            {column}
+                                                        </TableCell>
+                                                    ))}
+                                                </TableRow>
+                                            </TableHead>
+                                            <TableBody>
+                                                {this.state.users.slice((this.state.page - 1) * this.state.rowsPerPage,
+                                                    this.state.page * this.state.rowsPerPage).map((row, index) => {
+                                                    return (
+
+                                                        <TableRow component="tr" key={row.id}>
+                                                            <TableCell align={"center"}
+                                                                       component="td"
+                                                                       scope="row">
+                                                                {index + ((this.state.page - 1) * this.state.rowsPerPage) + 1}
+                                                            </TableCell>
+
+                                                            <TableCell
+                                                                align={"center"}>{row.username}
+                                                            </TableCell>
+
+                                                            <TableCell
+                                                                align={"center"}>{row.email}
+                                                            </TableCell>
+                                                        </TableRow>
+                                                    )
+                                                })
+                                                }
+                                            </TableBody>
+                                        </Table>
+                                    </TableContainer>
+                                    <TablePagination
+                                        rowsPerPageOptions={[1, 2, 3, 4, 5, 10, 25]}
+                                        component="div"
+                                        count={this.state.users.length}
+                                        rowsPerPage={this.state.rowsPerPage}
+                                        page={this.state.page - 1}
+                                        onChangePage={this.handleChangePage}
+                                        onChangeRowsPerPage={this.handleChangeRowsPerPage}
+                                    />
+                                </Paper>
+                                : <h2>Group has no members</h2>}
+                        </CardContent>
+                    </Card>
+                </div>
+            </div>
         );
     }
 }
